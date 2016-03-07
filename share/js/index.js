bddd5a893de5249cb8e02c0645f4eb0bba75f541
cwen <yansiwen@heungkong.com>
2016-03-07 15:29:21 +0800

    增加豆瓣分享

--- share/js/index.js
+++ share/js/index.js
@@ -14,17 +14,18 @@
 		var target = event.target;
 		if(target.tagName.toLowerCase() === "i") {
 			flag = target.className && target.className.split("-")[1];
-			console.log(flag);
 			switch(flag) {
 				case "qq": 
-					window.open("http://connect.qq.com/widget/shareqq/index.html?url=" + encodeURIComponent(shareConfig.url) + "&title=" + encodeURIComponent(shareConfig.title));
-					// window.open("http://connect.qq.com/widget/shareqq/index.html?" + s.join("&"));
+					window.open("http://connect.qq.com/widget/shareqq/index.html?" + s.join("&"));
 					break;
 				case "qzone":
 					window.open("http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?" + s.join("&"));
 					break;
 				case "wb":
 					window.open("http://service.weibo.com/share/share.php?" + s.join("&"));
+					break;
+				case "douban":
+					window.open("http://widget.renren.com/dialog/share?" + s.join("&"));
 					break;
 				default: break;
 			}
